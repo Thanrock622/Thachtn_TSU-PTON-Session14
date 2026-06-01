f48be8d5375630ddbc0d85a28a7d7226f523f7a4
Thanrock622 <91062673+Thanrock622@users.noreply.github.com>
2026-06-02 03:40:41 +0700

    Add files via upload

---
+++ Assignment 1/Assignment 1.js
@@ -0,0 +1,8 @@
+// tạo đối tượng
+let person = {
+    name: "John Doe",
+    age: 30,
+    job: "Developer",
+}
+console.log(person.name);//truy cập tên
+console.log(person); // in ra toàn bộ đối tượng
