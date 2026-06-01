53092a5697403e235c8926f18474e43ff28f1c4e
Thanrock622 <91062673+Thanrock622@users.noreply.github.com>
2026-06-02 03:50:33 +0700

    Add files via upload

---
+++ Assignment 2/Assignment2.js
@@ -0,0 +1,8 @@
+let car ={
+  brand: "Toyota",
+  model: "Corolla",
+  year: 2020,
+};
+car.color = "Red"; // thêm thuộc tính color vào đối tượng car
+car.year = 2022; // cập nhật giá trị của thuộc tính year thành 2022
+console.log(car);
